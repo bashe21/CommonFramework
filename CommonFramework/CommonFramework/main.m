f706b638f4ed843f14d0dcb1b1595e7005c85849
跋涉 <3064662835@qq.com>
2018-05-13 18:24:37 +0800

    创建工程

---
+++ CommonFramework/CommonFramework/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CommonFramework
+//
+//  Created by 张哈哈 on 2018/5/13.
+//  Copyright © 2018年 zyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
